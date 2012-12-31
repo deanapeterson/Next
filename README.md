d7392502a400907480facf050f7597137311c6e9
Dean Peterson <dean@iconwebdesigns.net>
2012-12-30 20:48:03 -0600

    README, console.html, next.js
    Added description and usage examples to README.md
    
    Created console.html for testing purposes.
    
    Added test for array
    Converted index reset to ternary statement instead of if/else

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 #Next
+Simple helper javascript object that provides the 'next' item in the array. Starting over after returning the last item in the array.
+
+Can return strings, numbers, and objects.
+
+##Usage
+
+###Strings
+<pre>
+var beatles = new Next( "John Paul George Ringo".split(" ") );
+
+beatles.next(); //John
+beatles.next(); //Paul
+beatles.next(); //George
+beatles.next(); //Ringo
+beatles.next(); //John
+</pre>
+
+###Numbers
+<pre>
+var num = new Next([1, 3, 6, 11]);
+
+num.next(); //1
+num.next(); //3
+num.next(); //6
+num.next(); //11
+num.next(); //1
+</pre>
+
+###Objects
+<pre>
+var obj = new Next([
+		{firstName : "Dave", lastName : "Matthews"},
+		{firstName : "John", lastName : "Mayer"},
+		{firstName : "Gordon", lastName : "Sumner" }
+	]);
+
+var item;
+
+for(var i = 0; i &lt; 3; i = i + 1){
+	item = obj.next();
+	return item.firstName + " " + item.lastName;
+}
+
+//Dave Matthews
+//John Mayer
+//Gordon Sumner
+
+</pre>
--- next.js
+++ next.js
@@ -3,50 +3,30 @@
 //provides convenient way to return an array of strings
 //Allows for multiple instances
 
-function Next(dataSource) {
-    "use strict";
-    this.stringArray = (function() {
-        //test source is string and convert to array
-        if (Object.prototype.toString.call(dataSource) !== '[object Array]') {
-            return dataSource.split(" ");
-        }
+function Next(itemList, options) {
+	"use strict";
+	options = options || {};
 
-        if (Object.prototype.toString.call(dataSource) === '[object Array]') {
-            return dataSource;
-        }
+	if( Object.prototype.toString.call( itemList ) !== '[object Array]' ) {
+		throw new Error("An array is required.");
+	}
 
-    }());
-    this.len = this.stringArray.length - 1;
-    this.index = 0;
+	this.original = itemList;
+	this.itemList = itemList.slice(0);
+	this.len = this.itemList.length - 1;
+	this.index = 0;
 }
+
 Next.prototype = {
-    next: function() {
-        "use strict";
-        var strReturn = this.stringArray[this.index];
-        //increment or reset index
-        if (this.index >= this.len) {
-            this.index = 0;
-        } else {
-            this.index = this.index + 1;
-        }
-        return strReturn;
-    }
+	next: function(options) {
+		"use strict";
+		options = options || {};
+
+		var item = this.itemList[this.index];
+
+		//increment or reset index
+		this.index = this.index >= this.len ? 0 : this.index + 1;
+
+		return item;
+	}
 };
-
-//test and usage
-var msg = function(text) {
-    var $result = $("#result"),
-        $div = $("<div></div>").text(text).appendTo($result);
-},
-    testArray = ["1", "2", "3", "4", "5", "6"],
-    testString = "one two three four";
-
-
-
-var CycleA = new Next(testString),
-    CycleB = new Next(testArray);
-
-for (var i = 0, len = 8; i < len; i = i + 1) {
-    msg("CycleA : " + CycleA.next() + " CycleB : " + CycleB.next());
-    msg("");
-};​
